d92a654b73edaee78683783bba11fdcc43cd4daa
Samuel Spink <dr.greenjet@gmail.com>
2025-03-04 16:19:33 +0100

    feat(Header): links config

---
+++ src/lib/links.ts
@@ -0,0 +1,21 @@
+interface Link {
+    name: string;
+    path: string;
+}
+
+const links: Link[] = [
+    {
+        name: "Home",
+        path: "/"
+    },
+    {
+        name: "Train",
+        path: "/train"
+    },
+    {
+        name: "History",
+        path: "/history"
+    },
+];
+
+export default links;
