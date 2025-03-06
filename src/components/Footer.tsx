79a253b714a538988e3dfdb890948fe1f4fb046f
Samuel Spink <dr.greenjet@gmail.com>
2025-03-06 09:41:23 +0100

    docs: reformat code style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,19 @@
 import Link from "next/link";
 
 export default function Footer() {
-  return (
-    <footer className="bg-themecolor text-white mx-4 my-4 rounded-lg p-6 flex flex-col justify-center items-center gap-2">
-      <div className="flex flex-row gap-6 font-medium text-base">
-        <Link href="/privacy" className="hover:text-gray-300 transition-colors">
-          Datenschutz
-        </Link>
-        <Link href="/imprint" className="hover:text-gray-300 transition-colors">
-          Impressum
-        </Link>
-      </div>
-      <p className="text-sm font-medium text-gray-200 flex items-center gap-1">
-        © 2025 Kantonsschule Hottingen
-      </p>
-    </footer>
-  );
+    return (
+        <footer className="bg-themecolor text-white mx-4 my-4 rounded-lg p-6 flex flex-col justify-center items-center gap-2">
+            <div className="flex flex-row gap-6 font-medium text-base">
+                <Link href="/privacy" className="hover:text-gray-300 transition-colors">
+                    Datenschutz
+                </Link>
+                <Link href="/imprint" className="hover:text-gray-300 transition-colors">
+                    Impressum
+                </Link>
+            </div>
+            <p className="text-sm font-medium text-gray-200 flex items-center gap-1">
+                © 2025 Kantonsschule Hottingen
+            </p>
+        </footer>
+    );
 }
--- src/components/NextAuthProvider.tsx
+++ src/components/NextAuthProvider.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { SessionProvider } from "next-auth/react";
 
 function AuthProvider({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+    return <SessionProvider>{children}</SessionProvider>;
 }
 
 export default AuthProvider;
