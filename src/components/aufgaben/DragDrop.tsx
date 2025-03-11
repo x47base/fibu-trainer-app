f65973304d209cac530dfa9d150cfe07d266e011
Samuel Spink <dr.greenjet@gmail.com>
2025-03-11 13:31:32 +0100

    feat(files): aufgaben component dateien

---
+++ src/components/aufgaben/DragDrop.tsx
@@ -0,0 +1,27 @@
+
+
+export default function Create(taskId: number) {
+    // TODO: DATA Fetch
+
+    const placeholderData = {
+        
+    };
+    
+    return (<>
+        {/* TODO: UI
+            - Aufgabenstellung
+            - Aufgabe
+
+            Drag & Drop Library : https://dndkit.com/
+        */}
+    </>);
+};
+
+export function Validate(element: any) {
+    let isValid = false;
+    let taskId;
+
+    // TODO: Handling
+
+    return isValid;
+}
---
+++ src/components/aufgaben/MultipleChoice.tsx
@@ -0,0 +1,21 @@
+
+
+export default function Create(taskId: number) {
+    // TODO: DATA Fetch
+    
+    return (<>
+        {/* TODO: UI
+            - Aufgabenstellung
+            - Aufgabe
+        */}
+    </>);
+};
+
+export function Validate(element: any) {
+    let isValid = false;
+    let taskId;
+
+    // TODO: Handling
+
+    return isValid;
+}
